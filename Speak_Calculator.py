056e73d1e68302f97389e584e7c31e22d6be21a6
Himanshusharma007-stack <66436350+Himanshusharma007-stack@users.noreply.github.com>
2020-08-12 02:09:17 -0700

    Update Speak_Calculator.py

--- Speak_Calculator.py
+++ Speak_Calculator.py
@@ -12,28 +12,31 @@ if __name__ == '__main__':
     if z== "yes":
         print("Welcome Next Generation Calculator\nEnter the first number")
         speak("Welcome Next Generation Calculator, Enter the first number")
-        num1= input()
-        print("Enter second number")
+        num1= int(input())
+        print("Enter second number") 
         speak("Enter second number")
-        num2= input()
+        num2= int(input())
         print("Enter + for Addition, - for subtraction, / for division, * for multiplication and % for find remainder")
         speak("Enter plus for Addition, minus for subtraction, divide for division, star for multiplication and % for find remainder")
         y= input()
         if y== "+":
-            print(f"Addition be {int(num1)+int(num2)}, have a nice day")
-            speak(f"Addition be {int(num1)+int(num2)}, have a nice day")
+            print(f"Addition be {num1+num2}, have a nice day")
+            speak(f"Addition be {num1+num2}, have a nice day")
         elif y== "-":
-            print(f"Subtraction be {int(num1)-int(num2)}, have a nice day")
-            speak(f"Subtraction be {int(num1)-int(num2)}, have a nice day")
+            print(f"Subtraction be {num1-num2}, have a nice day")
+            speak(f"Subtraction be {num1-num2}, have a nice day")
         elif y== "*":
-            print(f"Multiplication be {int(num1)*int(num2)}, have a nice day")
-            speak(f"Multiplication be {int(num1)*int(num2)}, have a nice day")
+            print(f"Multiplication be {num1*num2}, have a nice day")
+            speak(f"Multiplication be {num1*num2}, have a nice day")
         elif y== "/":
             print(f"Division be {float(num1)/float(num2)}, have a nice day")
             speak(f"Division be {float(num1)/float(num2)}, have a nice day")
         elif y== "%":
-            print(f"Remainder be {int(num1)%int(num2)}, have a nice day")
-            speak(f"Remainder be {int(num1)%int(num2)}, have a nice day")
+            print(f"Remainder be {num1%num2}, have a nice day")
+            speak(f"Remainder be {num1%num2}, have a nice day")
+        else:
+            print("This is an invalid input, please try again later")
+            speak("This is an invalid input, please try again later")
     elif z== "no":
         print("Please take the permission of Mr. Himanshu and try again! Thank you")
         speak("Please take the permission of Mr. Himanshu and try again! Thank you")
